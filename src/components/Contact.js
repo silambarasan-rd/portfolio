66606c963aeb36cc7f7ab493e267fd091288ce32
Silambarasan R <silambu1821944@gmail.com>
2022-12-10 16:55:35 +0530

    Contact with Map & Recommend btn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,94 +7,119 @@ import StackOverflowIcon from '../images/contact/contact-stack-overflow.svg';
 import LinkedInIcon from '../images/contact/contact-linkedin.svg';
 import MobileIcon from '../images/contact/contact-mobile.png';
 import Scroll from 'react-scroll';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
   return (
     <Scroll.Element name="contact-section">
-      <div className="contact-section">
-        <div className="container">
-          <div className="row">
-            <div className="col">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="contact-section">
               <SectionTitle sectionType="contact" />
 
               <div className="contact-container">
                 <div className="row justify-content-center">
                   <div className="col-md-4 col-12">
-                    <div className="contact-row">
-                      <div className="contact-image-container">
-                        <img src={MobileIcon}
-                          className="contact-image" alt="Mobile" />
-                      </div>
-                      <div className="contact-detail">
-                        <a href="tel: +91 77088 63236"
-                          target="_blank"
-                          rel="noreferrer"
-                          title="Send me a mail to silambu1821944@gmail.com">
-                          <h1>+91 77088 63236</h1>
-                        </a>
+                    <div className="contact-left-container">
+                      <div className="map-container">
+                        <iframe width="100%" className="map-frame"
+                          id="gmap_canvas"
+                          src="https://maps.google.com/maps?q=cuddalore&t=k&z=13&ie=UTF8&iwloc=&output=embed"
+                          frameBorder="0" scrolling="no"
+                          marginHeight="0" marginWidth="0" />
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-4 col-12">
-                    <div className="contact-row">
-                      <div className="contact-image-container">
-                        <img src={GmailIcon}
-                          className="contact-image" alt="Gmail" />
+                  <div className="col-md-8 col-12">
+                    <div className="contact-right-container">
+                      <div className="contact-details-wrapper">
+                        <div className="contact-row">
+                          <div className="contact-image-container">
+                            <img src={MobileIcon}
+                              className="contact-image" alt="Mobile" />
+                          </div>
+                          <div className="contact-detail">
+                            <a href="tel: +91 77088 63236"
+                              target="_blank"
+                              rel="noreferrer"
+                              title="Send me a mail to
+                              silambu1821944@gmail.com">
+                              <h1>+91 77088 63236</h1>
+                            </a>
+                          </div>
+                        </div>
+                        <div className="contact-row">
+                          <div className="contact-image-container">
+                            <img src={GmailIcon}
+                              className="contact-image" alt="Gmail" />
+                          </div>
+                          <div className="contact-detail">
+                            <a href="mailto: silambu1821944@gmail.com"
+                              target="_blank"
+                              rel="noreferrer"
+                              title="Send me a mail to
+                              silambu1821944@gmail.com">
+                              <h1>silambu1821944</h1>
+                            </a>
+                          </div>
+                        </div>
+                        <div className="contact-row">
+                          <div className="contact-image-container">
+                            <img src={GitHubIcon}
+                              className="contact-image" alt="GitHub" />
+                          </div>
+                          <div className="contact-detail">
+                            <a href="https://github.com/silambarasan-rd"
+                              target="_blank"
+                              rel="noreferrer"
+                              title="Visit my GitHub Profile">
+                              <h1>silambarasan-rd</h1>
+                            </a>
+                          </div>
+                        </div>
+                        <div className="contact-row">
+                          <div className="contact-image-container">
+                            <img src={StackOverflowIcon}
+                              className="contact-image" alt="Stack Overflow" />
+                          </div>
+                          <div className="contact-detail">
+                            <a href="https://stackoverflow.com/users/8192354/silambarasan-r-d"
+                              target="_blank"
+                              rel="noreferrer"
+                              title="Check out my Stack Overflow Profile">
+                              <h1>silambarasan-r-d</h1>
+                            </a>
+                          </div>
+                        </div>
+                        <div className="contact-row">
+                          <div className="contact-image-container">
+                            <img src={LinkedInIcon}
+                              className="contact-image" alt="LinkedIn" />
+                          </div>
+                          <div className="contact-detail">
+                            <a href="https://www.linkedin.com/in/silambarasan-rd"
+                              target="_blank"
+                              rel="noreferrer"
+                              title="Visit my LinkedIn Profile">
+                              <h1>silambarasan-rd</h1>
+                            </a>
+                          </div>
+                        </div>
                       </div>
-                      <div className="contact-detail">
-                        <a href="mailto: silambu1821944@gmail.com"
-                          target="_blank"
+                      <div className="contact-recommend-wrapper">
+                        <h2 className="recommend-caption">
+                          Wanna give kudos to him ?
+                        </h2>
+                        <a href="https://www.linkedin.com/in/silambarasan-rd/details/recommendations/"
                           rel="noreferrer"
-                          title="Send me a mail to silambu1821944@gmail.com">
-                          <h1>silambu1821944</h1>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-4 col-12">
-                    <div className="contact-row">
-                      <div className="contact-image-container">
-                        <img src={GitHubIcon}
-                          className="contact-image" alt="GitHub" />
-                      </div>
-                      <div className="contact-detail">
-                        <a href="https://github.com/silambarasan-rd"
                           target="_blank"
-                          rel="noreferrer"
-                          title="Visit my GitHub Profile">
-                          <h1>silambarasan-rd</h1>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-4 col-12">
-                    <div className="contact-row">
-                      <div className="contact-image-container">
-                        <img src={StackOverflowIcon}
-                          className="contact-image" alt="Stack Overflow" />
-                      </div>
-                      <div className="contact-detail">
-                        <a href="https://stackoverflow.com/users/8192354/silambarasan-r-d"
-                          target="_blank"
-                          rel="noreferrer"
-                          title="Check out my Stack Overflow Profile">
-                          <h1>silambarasan-r-d</h1>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-4 col-12">
-                    <div className="contact-row">
-                      <div className="contact-image-container">
-                        <img src={LinkedInIcon}
-                          className="contact-image" alt="LinkedIn" />
-                      </div>
-                      <div className="contact-detail">
-                        <a href="https://www.linkedin.com/in/silambarasan-rd"
-                          target="_blank"
-                          rel="noreferrer"
-                          title="Visit my LinkedIn Profile">
-                          <h1>silambarasan-rd</h1>
+                          className="btn btn-lg btn-outline-dark recommend-btn">
+                          <FontAwesomeIcon icon={faLinkedin} />
+                          <span className="ms-2">
+                            Recommend <strong>Silambarasan R</strong>
+                          </span>
                         </a>
                       </div>
                     </div>
